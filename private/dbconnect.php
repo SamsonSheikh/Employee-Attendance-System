5f71e4fcab596e07a19c5b0da5e6f889a1920d35
SamsonSheikh <apeeezsamson16@gmail.com>
2026-06-07 18:33:58 +0800

    Create dbconnect.php

---
+++ private/dbconnect.php
@@ -0,0 +1,9 @@
+<?php
+// config.php - LOCAL MACHINE ONLY (Ignored by Git)
+
+// Default XAMPP settings
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', ''); // Leave blank if you haven't set a XAMPP MySQL password
+define('DB_NAME', 'EmAttendancedb');
+?>
